c8b9a6a3e5e302a329906ac1e99b841a26523dec
matthew <mattk4@me.com>
2024-05-03 13:17:45 -0700

    updated styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,14 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      colors: {
+        // facebook colors
+        fb: {
+          blue: "#4267B2",
+          grey: "#898F9C",
+          black: "#000000",
+        },
+      },
     },
   },
   plugins: [],
